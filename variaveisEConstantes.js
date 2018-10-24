112e39866b068eb6f0b1105416085a726818b8dc
aschaves1976 <roots17@gmail.com>
2018-10-24 15:38:44 -0300

    Fundamentos - O básico de Variáveis e Constantes ( Var, Let e Const)

---
+++ variaveisEConstantes.js
@@ -0,0 +1,18 @@
+var n1 = 3;
+console.log(n1)
+/**
+ * existe diferenca entre criar uma variável
+ * utilizando "var" e "let".
+ * 
+ * De preferência para a utilização com let,
+ * pois é uma forma mais moderna de criar variáveis e tem algumas regras a mais do que utilizando VAR
+ */
+let n2 = 6
+console.log(n2);
+let s1 ="Maria"
+console.log(s1)
+console.log(n1+n2)
+
+const c1 = 1000
+n2 = c1
+console.log(n1,n2,s1)
