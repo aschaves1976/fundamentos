44ca785f42b3a10d75642cca066ca53c6f093a43
aschaves1976 <roots17@gmail.com>
2018-10-24 17:42:55 -0300

    Fundamentos - Tipo String

---
+++ string.js
@@ -0,0 +1,14 @@
+const escola = "Coood3r"
+console.log (escola)
+console.log (escola.charAt(4))
+console.log(escola.length)
+console.log(escola.charCodeAt(3)) // retorna o valor da tabela ASCII
+console.log (escola.indexOf('d')) //retorna a posição da letra dentro da string
+console.log (escola.lastIndexOf('o')) // retona a última posição de um determinado caracter dentro da string
+console.log (escola.substr(1,4))
+console.log (escola.substring(1))
+console.log (escola.substring(1,4))
+console.log ('Escola '.concat(escola).concat("!"))
+console.log(escola.replace('ooo', 'o').replace('3','e'))
+console.log('Maria, João, Ana'.split(',')) // gera um array
+
