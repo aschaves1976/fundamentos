78f0a15d2928a3c391a671b6802ab2ffddcda9e5
aschaves1976 <roots17@gmail.com>
2018-10-29 18:01:46 -0300

    Fundamentos - Declaração de Variáveis com Var #01

---
+++ usandoVar1.js
@@ -0,0 +1,11 @@
+{ { { { var sera = 'Será???' } } } }
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log(local)
+}
+
+teste()
+//console.log(local) // erro pois a variavel esta definida dentro do escopo da função
