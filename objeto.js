59c9b8ffe7cc58983be5f71f4c9a1520310fbae3
aschaves1976 <roots17@gmail.com>
2018-10-29 15:13:25 -0300

    Fundamentos - Object

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const prod1 ={} // o par de chaves representa um objeto
+prod1.nome = 'Notebook Ultra Fino'
+prod1.valor = 4500.0
+
+console.log(prod1)
+
+const prod2 ={
+    nome: 'Camisa Polo',
+    valor: 79.90
+}
+console.log(prod2)
