7da9556aa75cb53ebcdc4aff1fea7b4423df2734
aschaves1976 <roots17@gmail.com>
2018-10-31 16:57:17 -0300

    Fundamentos - Operadores: Atribuição

---
+++ atribuicao.js
@@ -0,0 +1,16 @@
+const a = 7
+let b = 3
+b +=a  // utilização da atribuição aditiva. Esta sendo acrescentado o valor de a em b
+console.log('Resultado de b += a: ', b)
+b -= 4
+console.log('Resultado de b -= 4: ', b)
+b *= 2
+console.log('Resultado de b *= 2: ', b)
+b /= 2
+console.log('Resultado de b /= 2: ', b)
+b %= 2 // resto da divisão de b / 2
+console.log('Resultado de b %= 2: ', b)
+
+
+
+
